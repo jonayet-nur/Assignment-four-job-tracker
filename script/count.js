c1f387648a366964ad527613c372e3672dd28b52
jonayet-nur <jonayetnur@gmail.com>
2026-02-24 10:37:44 +0600

    some problem fixed

--- script/count.js
+++ script/count.js
@@ -18,8 +18,31 @@ function totalCount(){
     interviewCount.innerText = interviewList.length;
 
     //jokon reject button e click korbo tokon  oi ta reject  list e push hobe
-    
+
     rejectedCount = rejectedList.length;
     
 }
 totalCount()
+
+
+mainContainer.addEventListener("click" , function(event){
+    const parenNode = event.target.parentNode.parentNode
+    
+    // job: job-companies,job-title,job-info,job-status, job-description,interview-btn,reject-btn, card-right,
+    const jobComapany = parenNode.querySelector('.job-companies').innerText;
+    const jobTittle = parenNode.querySelector('.job-title').innerText;
+    const jobInfo = parenNode.querySelector('.job-info').innerText;
+    const jobStatus = parenNode.querySelector('.job-status').innerText;
+    const jobDescription = parenNode.querySelector('.job-description').innerText;
+
+    const cardInfo = {
+        jobComapany,
+        jobTittle,
+        jobInfo,
+        jobStatus,
+        jobDescription
+    }
+    
+    
+
+})
