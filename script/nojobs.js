2093e432b82b4353c0e9f182dc96b50910f2e1d1
jonayet-nur <jonayetnur@gmail.com>
2026-02-24 22:23:24 +0600

    no jobs card section problem fixed

--- script/nojobs.js
+++ script/nojobs.js
@@ -1,22 +1,31 @@
-  //no jobs interview function
-  function interviewRender(){
+  
+//  Interview Render Fixed no jobs card
+
+function interviewRender() {
     filterSection.innerHTML = '';
 
-    const emptySection = document.getElementById('interview-empty');
+    // hide rejected empty
+    const rejectedEmpty = document.getElementById('rejected-empty');
+    if(rejectedEmpty) rejectedEmpty.classList.add('hidden');
 
     if(interviewList.length === 0){
-        emptySection.classList.remove('hidden');
+        const div = document.createElement('div');
+        div.className = "no-jobs w-10/12 mx-auto mt-4";
+        div.innerHTML =`
+            <div class="text-center bg-white py-20 rounded-md border-gray-200 shadow-md"> 
+                <img src="images/jobs.png" alt="" class="mx-auto mb-5">
+                <h2 class="mb-1 font-semibold text-2xl text-[#002C5C]">No jobs available</h2>
+                <p class="text-lg text-[#64748B]">Check back soon for new job opportunities</p>
+            </div>
+        `
+        filterSection.appendChild(div);
         return;
-    }else{
-        emptySection.classList.add('hidden');
     }
 
     for(let interview of interviewList){
         const div = document.createElement('div');
-        div.className = 'bg-white border border-gray-200 shadow-sm p-8 rounded-md flex justify-between hover:shadow-lg mb-4'
-          
-         div.innerHTML = `
-         <div class="card-top space-y-5">
+        div.className = 'bg-white border border-gray-200 shadow-sm p-8 rounded-md flex justify-between hover:shadow-lg mb-4';
+        div.innerHTML = `<div class="card-top space-y-5">
             <h2 class=" job-companies text-xl font-semibold"> ${interview.jobComapany} </h2>
             <h5 class=" job-title text-neutral/50">${interview.jobTittle}</h5>
             <p class=" job-info text-neutral/50">${interview.jobInfo}</p>
@@ -30,56 +39,59 @@
          <div class=" card-right  w-8 h-8 flex justify-center cursor-pointer items-center border border-gray-200 rounded-full shrink-0">
                 <img src="images/Trash.png" alt="">
             </div>
-        
+       
         ` 
-
+         
         filterSection.appendChild(div);
     }
-
 }
 
-
-
-
-//no jobs rejected function
-function rejectRender(){
+//  Rejected Render Fixed no jobs card 
+function rejectRender() {
     filterSection.innerHTML = '';
 
-    const emptySection = document.getElementById('interview-empty');
+    // hide interview empty
+    const interviewEmpty = document.getElementById('interview-empty');
+    if(interviewEmpty) interviewEmpty.classList.add('hidden');
 
     if(rejectedList.length === 0){
-        emptySection.classList.remove('hidden');
+        const div = document.createElement('div');
+        div.className = "no-jobs w-10/12 mx-auto mt-4";
+        div.innerHTML = `
+            <div class="text-center bg-white py-20 rounded-md border-gray-200 shadow-md"> 
+                <img src="images/jobs.png" alt="" class="mx-auto mb-5">
+                <h2 class="mb-1 font-semibold text-2xl text-[#002C5C]">No jobs available</h2>
+                <p class="text-lg text-[#64748B]">Check back soon for new job opportunities</p>
+            </div>
+        `
+        
+        filterSection.appendChild(div);
         return;
-    }else{
-        emptySection.classList.add('hidden');
     }
 
     for(let interview of rejectedList){
         const div = document.createElement('div');
-        div.className = 'bg-white border border-gray-200 shadow-sm p-8 rounded-md flex justify-between hover:shadow-lg mb-4'
-          
-         div.innerHTML = `
+        div.className = 'bg-white border border-gray-200 shadow-sm p-8 rounded-md flex justify-between hover:shadow-lg mb-4';
+        div.innerHTML = `
          <div class="card-top space-y-5">
-            <h2 class=" job-companies text-xl font-semibold"> ${interview.jobComapany} </h2>
-            <h5 class=" job-title text-neutral/50">${interview.jobTittle}</h5>
-            <p class=" job-info text-neutral/50">${interview.jobInfo}</p>
-            <p class=" job-status inline-block px-4 py-2 rounded bg-blue-100 text-blue-700 text-xs font-semibold">${interview.jobStatus}</p>
+            <h2 class="job-companies text-xl font-semibold">${interview.jobComapany}</h2>
+            <h5 class="job-title text-neutral/50">${interview.jobTittle}</h5>
+            <p class="job-info text-neutral/50">${interview.jobInfo}</p>
+            <p class="job-status inline-block px-4 py-2 rounded bg-blue-100 text-blue-700 text-xs font-semibold">${interview.jobStatus}</p>
             <p class="job-description">${interview.jobDescription}</p>
             <div class="flex gap-3">
-                <button class=" interview-btn  btn btn-sm btn-outline btn-success">Interview</button>
-                <button class=" reject-btn btn btn-sm btn-outline btn-error">Rejected</button>
+                <button class="interview-btn btn btn-sm btn-outline btn-success">Interview</button>
+                <button class="reject-btn btn btn-sm btn-outline btn-error">Rejected</button>
             </div>
         </div>
-         <div class=" card-right  w-8 h-8 flex justify-center cursor-pointer items-center border border-gray-200 rounded-full shrink-0">
-                <img src="images/Trash.png" alt="">
-            </div>
-        
-        ` 
-
+         <div class="card-right w-8 h-8 flex justify-center cursor-pointer items-center border border-gray-200 rounded-full shrink-0">
+            <img src="images/Trash.png" alt="">
+        </div>
+        `;
         filterSection.appendChild(div);
     }
-
 }
+
 
 
 // out of  8 jobs count korar jonno 
